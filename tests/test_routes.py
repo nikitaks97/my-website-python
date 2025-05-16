2a8f29b3f7f34e808f521c09f3bdd68385c42b0e
nikitaks97 <nikitaks997@gmail.com>
2025-05-16 15:19:24 +0200

    hb

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,5 +1,11 @@
 from flask import Flask
 import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
 
 def test_index(client):
     response = client.get('/')
